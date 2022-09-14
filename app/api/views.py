a9c45f0d83b9e54abba796546382e25ad887ff01
Kislov Dmitry <kislov.urfu98@gmail.com>
2022-09-14 16:27:14 +0500

    loguru

--- app/api/views.py
+++ app/api/views.py
@@ -1,14 +1,10 @@
 from flask import Blueprint, jsonify
 from app.posts.posts_dao import PostsDAO
 import logging
+from loguru import logger
 
-new_logger = logging.getLogger()
 
-formatter_one = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-file_handler = logging.FileHandler("api.log")
-file_handler.setFormatter(formatter_one)
-
-new_logger.addHandler(file_handler)
+logger.add('logs/api.log', format="{time} {level} {message}", level="INFO")
 
 api_blueprint = Blueprint('api_blueprint', __name__)
 
@@ -17,7 +13,7 @@ api_blueprint = Blueprint('api_blueprint', __name__)
 def get_posts_for_api():
     data = PostsDAO()
     post_data = data.get_posts_all()
-    new_logger.info(f'Запрос получения постов')
+    logger.info(f'Запрос получения постов')
     return jsonify(post_data)
 
 
@@ -25,7 +21,7 @@ def get_posts_for_api():
 def get_one_post_for_api(post_id):
     data = PostsDAO()
     post_data = data.get_post_by_pk(post_id)
-    new_logger.info(f'Запрос получения поста под номером {post_id}')
+    logger.info(f'Запрос получения поста под номером {post_id}')
     return jsonify(post_data)
 
 
